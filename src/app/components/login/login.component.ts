a00316c129c692da872ed7dd9e1ca8b48832ab1c
youga11 <youga.dieng@yahoo.fr>
2019-08-16 17:31:12 +0000

    login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -6,19 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+
+
   clavier_val: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   username;
   password;
   rows = [0, 1, 2, 3, 4, 5];
   b = [];
   input_clavier = '';
-  constructor() {
+  constructor(private formBuilder: FormBuilder, private router: Router) {
     for (let i = 0; i < 6; i++) {
       for (let y = 0; y < 5; y++) {
-        let e = Math.floor( Math.random() * 1000 );
+        let e = Math.floor(Math.random() * 1000);
         console.log(e);
         // tslint:disable-next-line:triple-equals
-        if ( (e % 2) == 0) {
+        if ((e % 2) == 0) {
           if (this.clavier_val.length > 0) {
             this.b.push(this.clavier_val.shift());
           } else {
@@ -30,7 +35,7 @@ export class LoginComponent implements OnInit {
       }
     }
     console.log(this.b);
-   }
+  }
 
 
   ngOnInit() {
@@ -39,5 +44,17 @@ export class LoginComponent implements OnInit {
   setInputClavier(val) {
     this.input_clavier = this.input_clavier + val;
   }
+  login(event) {
+    console.log(this.username);
+    console.log(this.password);
+    if (this.username == "123456" && this.input_clavier== "123456") {
+      this.router.navigate(['dashboard']);
 
+    }
+    else {
+      window.alert('Authentificationpas correcte');
+
+
+    }
+  }
 }
