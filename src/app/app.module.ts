0d0f0f1a1431d392a7119c1cc1d8f2ea6267f0da
ibouraimacoulibaly <iboucoulibaly92@gmail.com>
2019-08-19 10:36:23 +0000

    message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import {MatRadioModule} from '@angular/material/radio';
 
 import { InternationalPhoneNumberModule } from 'ngx-international-phone-number';
 import { AgmCoreModule } from '@agm/core';
-import { FileSelectDirective } from 'ng2-file-upload';
+// import { FileSelectDirective } from 'ng2-file-upload';
 import {MatDialogModule} from "@angular/material";
 
 
@@ -31,7 +31,7 @@ import { PretsEnCoursComponent } from './prets/prets-en-cours/prets-en-cours.com
 import { NouveauPretsComponent } from './prets/nouveau-prets/nouveau-prets.component';
 import { RegistreComponent } from './registre/registre.component';
 import { RegistreFormComponent } from './registre-form/registre-form.component';
-import { VirementMasseComponent } from './virement-masse/virement-masse.component';
+
 import { ReleveIdentificationComponent } from './releve-identification/releve-identification.component';
 import { ReleveImpressionComponent } from './releve-impression/releve-impression.component';
 import { NouveauCompteComponent } from './components/dashboard/compte/nouveau-compte/nouveau-compte.component';
@@ -85,7 +85,7 @@ import { BeneficiaireNouveauComponent } from './beneficiaire-nouveau/beneficiair
     NouveauPretsComponent,
     RegistreComponent,
     RegistreFormComponent,
-    VirementMasseComponent,
+   
     ReleveIdentificationComponent,
     ReleveImpressionComponent,
     NouveauCompteComponent,
@@ -94,7 +94,7 @@ import { BeneficiaireNouveauComponent } from './beneficiaire-nouveau/beneficiair
     ReseauxGabComponent,
     MessagerieComponent,
     DeviseComponent,
-    FileSelectDirective,
+    //  FileSelectDirective,
     PresentationComponent,
     AbonnementComponent,
     ReinitiationMotDePasseComponent,
@@ -133,7 +133,7 @@ import { BeneficiaireNouveauComponent } from './beneficiaire-nouveau/beneficiair
     MatButtonModule,
     NgxPrintModule,
     AgmCoreModule.forRoot({
-     //apiKey: '8110477bc4msha7acb11d1bf06b4p1bc2cfjsn2595b3a79a31',
+    apiKey: '8110477bc4msha7acb11d1bf06b4p1bc2cfjsn2595b3a79a31',
       libraries: ['places']
     }),
     MatDialogModule
