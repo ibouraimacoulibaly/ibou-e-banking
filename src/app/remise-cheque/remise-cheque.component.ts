814abd6d008f0aa1e2c0d5d50923b20de3426078
ibouraimacoulibaly <iboucoulibaly92@gmail.com>
2019-08-26 09:25:01 +0000

    message

--- src/app/remise-cheque/remise-cheque.component.ts
+++ src/app/remise-cheque/remise-cheque.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Input } from '@angular/compiler/src/core';
 
 @Component({
   selector: 'app-remise-cheque',
@@ -7,8 +8,16 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./remise-cheque.component.css']
 })
 export class RemiseChequeComponent implements OnInit {
-  number = new FormControl('');
-  number1 = new FormControl('');
+  number = new FormControl();
+  number1 = new FormControl();
+ //resultat = new FormControl();
+//  number: number = 10;  
+//   number1: number = 20; 
+  total;
+  list_Array =[];
+  sum;
+ 
+
  
   constructor() { }
 
@@ -18,7 +27,8 @@ export class RemiseChequeComponent implements OnInit {
   newAttribute: any = {};
 
   firstField = true;
-  firstFieldName = 'Emetteur';
+firstFieldName = 'Emetteur';
+  
   // isEditItems: boolean;
 
   addFieldValue(index) {
@@ -37,5 +47,26 @@ export class RemiseChequeComponent implements OnInit {
   onEditCloseItems() {
    // this.isEditItems = !this.isEditItems;
   }
-  
+    calcul() {
+       
+    let list = document.querySelectorAll("[data-montant]");
+    this.total = 0;
+    this.list_Array= [];
+     list.forEach((element:HTMLInputElement) => {
+      this.total = element.value;
+      this.list_Array.push(element.value);
+      
+    });
+     
+   }
+
+   afterAdd(){
+    let list: NodeListOf<HTMLInputElement> = document.querySelectorAll("[data-montant]");
+    for( let i=0 ; i < this.list_Array.length; i++ ){
+      list[i].value= this.list_Array[i];
+     }
+   }
+   somme(){
+    this.sum = this.total + this.list_Array;
+   }
 }
